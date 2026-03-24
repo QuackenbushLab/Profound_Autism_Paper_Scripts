2e7692a544738eb0f04ae9b197a039a43f221fad
Tara Eicher <taraeicher@yahoo.com>
2026-03-24 16:31:52 -0700

    Modified filtering of trans results

--- processParquetResultsDiff.R
+++ processParquetResultsDiff.R
@@ -18,24 +18,22 @@ readAllCis <- function(fpath){
   str(parquetSig)
   return(parquetSig)
 }
-#profoundCis <- readAllCis(paste0(eQTLDir, "/profoundAllResults.cis_qtl_pairs"))
-#notProfoundCis <- readAllCis(paste0(eQTLDir, "/notProfoundAllResults.cis_qtl_pairs"))
 profoundBothCis <- readAllCis(paste0(eQTLDir, "/profoundBothDiffResults.cis_qtl_pairs"))
 profoundModerateIDCis <- readAllCis(paste0(eQTLDir, "/profoundModerateIDDiffResults.cis_qtl_pairs"))
-profoundNonverbalCis <- readAllCis(paste0(eQTLDir, "/profoundNonverbalDiffResults.cis_qtl_pairs"))
 verbalGiftedCis <- readAllCis(paste0(eQTLDir, "/verbalGiftedDiffResults.cis_qtl_pairs"))
 verbalNoIDCis <- readAllCis(paste0(eQTLDir, "/verbalNoIDDiffResults.cis_qtl_pairs"))
 verbalMildCis <- readAllCis(paste0(eQTLDir, "/verbalMildIDDiffResults.cis_qtl_pairs"))
 
+M <- 34070 * 2440283
 getAboveCutoff <- function(file){
-  padj <- p.adjust(file$pval, method = "fdr")
-  return(file[which(padj < cutoff),])
+  ranks <- rank(file$pval, ties.method = "first")
+  padj <- file$pval * (M / ranks)
+  sig <- file[which(padj < cutoff),]
+  str(sig)
+  return(sig)
 }
-#profoundTransSig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/profoundAllResultsTrans.trans_qtl_pairs.parquet")))
 profoundBothSig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/profoundBothDiffTransResults.trans_qtl_pairs.parquet")))
-profoundNonverbalOnlySig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/profoundNonverbalDiffTransResults.trans_qtl_pairs.parquet")))
 profoundModerateIDOnlySig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/profoundModerateIDDiffTransResults.trans_qtl_pairs.parquet")))
-#notProfoundTransSig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/notProfoundAllResultsTrans.trans_qtl_pairs.parquet")))
 verbalGiftedTransSig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/verbalGiftedDiffTransResults.trans_qtl_pairs.parquet")))
 verbalNoIDTransSig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/verbalNoIDDiffTransResults.trans_qtl_pairs.parquet")))
 verbalMildIDTransSig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/verbalMildIDDiffTransResults.trans_qtl_pairs.parquet")))
@@ -44,37 +42,25 @@ verbalMildIDTransSig <- getAboveCutoff(arrow::read_parquet(paste0(eQTLDir, "/ver
 getPairs <- function(result){
   return(paste(result$variant_id, result$phenotype_id, sep = "__"))
 }
-#pairsProfoundCis <- getPairs(profoundCis)
-#pairsNotProfoundCis <- getPairs(notProfoundCis)
-#pairsProfoundTrans <- getPairs(profoundTransSig)
-#pairsNotProfoundTrans <- getPairs(notProfoundTransSig)
 
 pairsProfoundBothCis <- getPairs(profoundBothCis)
-pairsProfoundNonverbalCis <- getPairs(profoundNonverbalCis)
 pairsProfoundModerateIDCis <- getPairs(profoundModerateIDCis)
 pairsGiftedCis <- getPairs(verbalGiftedCis)
 pairsNoIDCis <- getPairs(verbalNoIDCis)
 pairsMildIDCis <- getPairs(verbalMildCis)
 
 pairsProfoundBothTransSig <- getPairs(profoundBothSig)
-pairsProfoundNonverbalTransSig <- getPairs(profoundNonverbalOnlySig)
 pairsProfoundModerateIDTransSig <- getPairs(profoundModerateIDOnlySig)
 pairsGiftedTransSig <- getPairs(verbalGiftedTransSig)
 pairsNoIDTransSig <- getPairs(verbalNoIDTransSig)
 pairsMildIDTransSig <- getPairs(verbalMildIDTransSig)
 
-saveRDS(list(#pairsProfoundCis = pairsProfoundCis,
-             #pairsNotProfoundCis = pairsNotProfoundCis,
-             #pairsProfoundTrans = pairsProfoundTrans,
-             #pairsNotProfoundTrans = pairsNotProfoundTrans,
-             pairsProfoundBothCis = pairsProfoundBothCis,
-             pairsProfoundNonverbalCis = pairsProfoundNonverbalCis,
+saveRDS(list(pairsProfoundBothCis = pairsProfoundBothCis,
              pairsProfoundModerateIDCis = pairsProfoundModerateIDCis,
              pairsGiftedCis = pairsGiftedCis,
              pairsNoIDCis = pairsNoIDCis,
              pairsMildIDCis = pairsMildIDCis,
              pairsProfoundBothTransSig = pairsProfoundBothTransSig,
-             pairsProfoundNonverbalTransSig = pairsProfoundNonverbalTransSig,
              pairsProfoundModerateIDTransSig = pairsProfoundModerateIDTransSig,
              pairsGiftedTransSig = pairsGiftedTransSig,
              pairsNoIDTransSig = pairsNoIDTransSig,
